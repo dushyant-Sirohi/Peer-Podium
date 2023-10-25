6fbbf323368ddad6cadbbcc296180ed4da9b131a
Dushyant Sirohi <dushyantsirohi217@gmail.com>
2023-10-25 16:35:35 -0400

    Fix lost connection issue

--- backend/actions.js
+++ backend/actions.js
@@ -7,6 +7,9 @@ const ACTIONS = {
     RELAY_SDP: 'relay-sdp',
     SESSION_DESCRIPTION: 'session-description',
     ICE_CANDIDATE: 'ice-candidate',
+    MUTE: 'mute',
+    UNMUTE: 'unmute',
+    MUTE_INFO: 'mute-info',
 };
 
 module.exports = ACTIONS;
